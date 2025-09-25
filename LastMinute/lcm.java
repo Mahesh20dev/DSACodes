48868bd9200f391d2746b14c4260088b48d63bc7
Mahesh20dev <ycmahesh86@gmail.com>
2025-09-25 22:59:02 +0530

    .

--- LastMinute/lcm.java
+++ LastMinute/lcm.java
@@ -2,8 +2,11 @@ public class lcm {
     int x = 12, y = 18;
     int gcd = 1;
     for(int i = 1; i <= x && i <= y; i++) {
-    if(x % i == 0 && y % i == 0) gcd = i;
+        if(x % i == 0 && y % i == 0) gcd = i;
     }
+
     int lcm = (x * y) / gcd;
     System.out.println("LCM: " + lcm);
 }
+//tc: O(min(x,y))
+//sc: O(1)
