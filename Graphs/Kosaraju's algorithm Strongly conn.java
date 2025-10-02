7ffc0124f0e26284a3208c26bb7797d1bfcaa448
Mahesh20dev <ycmahesh86@gmail.com>
2025-10-02 22:26:33 +0530

    .

--- Graphs/Kosaraju's algorithm Strongly conn.java
+++ Graphs/Kosaraju's algorithm Strongly conn.java
@@ -71,7 +71,7 @@ public class OnceAgain {
         }
 
         for(int i=0; i<V; i++) {
-            for(int j=0; j<graph[i].size(); i++) {
+            for(int j=0; j<graph[i].size(); j++) {
                 Edge e = graph[i].get(j);
                 transpose[e.dest].add(new Edge(e.dest, e.src));  // reversing edge
             }
